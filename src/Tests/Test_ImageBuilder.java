45472426156f3df912ea961bdec56fdaa811d095
mi5ho <michal.art3@gmail.com>
2017-05-04 09:28:31 +0100

    CombatManager update

---
+++ src/Tests/Test_ImageBuilder.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import CombatLearning.ImageBuilder;
+
+/**
+ * Created by Misho on 3.5.2017.
+ */
+public class Test_ImageBuilder {
+
+    public static void main(String[] args) {
+        ImageBuilder imgbld = new ImageBuilder();
+        imgbld.createImage();
+    }
+}
